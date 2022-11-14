9f57be9a468fe9a071eda7aabcb63bff51f9b38b
GitJG <cemileqarayeva93@gmail.com>
2022-11-15 01:40:56 +0400

    Initial commit

--- src/main/java/hw_StepProject/Main.java
+++ src/main/java/hw_StepProject/Main.java
@@ -3,6 +3,7 @@ package hw_StepProject;
 import java.text.ParseException;
 public class Main {
     public static void main(String[] args) throws ParseException {
+        //Run scripts
         BookingManager app = new BookingManager();
         app.run();
     }
